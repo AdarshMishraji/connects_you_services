3f06ebf9d9f6aa31a4a191b50f328d72c25541cd
Adarsh Mishra <adarshmishra969@gmail.com>
2022-11-05 01:51:36 +0530

    initialise server as client moved here

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import * as protoLoader from '@grpc/proto-loader';
 
 export type TServiceRecord = 'auth' | 'user' | 'room';
 
+export type TInitialiseServiceAsClientParams = {
+	service: any,
+	address: string,
+	deadlineSeconds?: number
+};
+
 export const ServicesRecord: Record<TServiceRecord, string> = {
 	auth: './proto/auth.proto',
 	user: './proto/user.proto',
@@ -22,5 +28,22 @@ export const getServiceProvider = (service: TServiceRecord) => {
 	return grpc.loadPackageDefinition(packageDef);
 };
 
+export const initialiseServiceAsClient = ({ service, address, deadlineSeconds = 5 }: TInitialiseServiceAsClientParams) => {
+	const client = new service(address, grpc.credentials.createInsecure(), {
+		'grpc.max_reconnect_backoff_ms': 10000,
+	});
+	
+	const deadline = new Date();
+	deadline.setSeconds(deadline.getSeconds() + deadlineSeconds);
+	
+	client.waitForReady(deadline, (error) => {
+		if (error) {
+			return console.log(`${service?.serviceName} ->> ${address} error`, error);
+		}
+		console.log(`${service?.serviceName} ->> ${address} is ready`);
+	});
+	return client;
+};
 
-export {grpc, protoLoader};
+
+export { grpc, protoLoader };
